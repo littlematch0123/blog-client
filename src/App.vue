afa61ed1d06ffc01f815735289b3adf99ec43d50
xiaohuochai <121631835@qq.com>
2018-06-15 03:17:42 +0800

    将全局默认样式内置到app.vue组件中

--- src/App.vue
+++ src/App.vue
@@ -87,15 +87,115 @@ export default {
 }
 </script>
 <style module lang="postcss">
-@import 'assets/style.css';
 html {
   height: 100%;
+  -webkit-text-size-adjust: 100%;
+  text-rendering: optimizelegibility;
+  -webkit-font-smoothing: antialiased;
+  background: #fafafa;
 }
 
 body {
   height: 100%;
+  overflow: hidden;
   background-image: linear-gradient(90deg, #f3f3f3 2px, transparent 0), linear-gradient(#f3f3f3 2px, transparent 0), linear-gradient(90deg, #f3f3f3 1px, transparent 0), linear-gradient(#f3f3f3 1px, transparent 0);
   background-size: 100px 100px, 100px 100px, 40px 40px, 40px 40px;
+}
+
+/* 格式化边距及边框 */
+body,
+p,
+h1,
+h2,
+h3,
+h4,
+dl,
+dd,
+figure {
+  margin: 0;
+}
+
+ol,
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+input,
+button {
+  padding: 0;
+  border: none;
+}
+
+fieldset,
+img {
+  border: 0;
+}
+
+/* 要注意表单元素并不继承父级 font 的问题 */
+body,
+button,
+input,
+select,
+textarea {
+  font: 12px/1.2 PingFang SC, Lantinghei SC, Microsoft Yahei, Hiragino Sans GB, Microsoft Sans Serif, WenQuanYi Micro Hei, sans-serif;
+}
+
+/* 电脑端初始字体大小为16px */
+@media all and (min-width: 900px) {
+  body,
+  button,
+  input,
+  select,
+  textarea {
+    font-size: 15px;
+  }
+}
+
+/* 去掉各Table cell 的边距并让其边重合 */
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+/* 默认不显示下划线，保持页面简洁 */
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+/* 表单格式化 */
+input {
+  -webkit-appearance: none;
+}
+
+button {
+  background: none;
+}
+
+/* 代码片断 */
+pre,
+code,
+pre tt {
+  font-family: Courier, 'Courier New', monospace;
+}
+
+/* 电脑端自定义滚动条 */
+@media all and (min-width: 900px) {
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, .1);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, .1);
+  }
 }
 
 .wrap {
